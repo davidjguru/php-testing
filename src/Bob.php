95383cc348401d1a4013d5e75260ca225bb8aad1
davidjguru <davidjguru@gmail.com>
2020-03-22 22:45:37 +0100

    (PHP) - First refactoring of the Bob.php class.

--- src/Bob.php
+++ src/Bob.php
@@ -9,42 +9,23 @@ class Bob {
         $response = 'Whatever.';
         if (empty($input) || ctype_space($input) || ctype_cntrl($input)) {
             $response = 'Fine. Be that way!';
-        } else if (strpos($input, '!') && (!strpos($input, '?'))) {
-            $modified_input = rtrim($input, '!');
-            $modified_input_2 = str_replace(' ', '', $modified_input);
-            if (ctype_upper($modified_input_2)) {
-                $response = 'Whoa, chill out!';
-            } else if(substr_count($input, '!') > 1) {
-                $response = 'Whoa, chill out!';
-            } else if(ctype_upper(substr($modified_input, -1, 2))) {
-                $response = 'Whoa, chill out!';
-            }
-              else {
-                $response = 'Whatever.';
-            }
-        } else if (ctype_upper($input)) {
-            $response = 'Whoa, chill out!';
 
+        } else if (strpos($input, '!') && (!strpos($input, '?'))) {
+            $modified_input = str_replace(' ', '', rtrim($input, '!'));
+
+            if (ctype_upper($modified_input) || (substr_count($input, '!') > 1)  || (ctype_upper(substr($modified_input, -1, 2)))) {
+                $response = 'Whoa, chill out!';
+            }
         } else if (((strlen($input)) - 1) == (strpos($input, '?'))) {
-            $length = strlen($input);
-            if (($length - 1) == strpos($input, '?')) {
-                $modified_input = rtrim($input, '?');
-                $modified_input_2 = str_replace(' ', '', $modified_input);
-                if (ctype_upper($modified_input_2)) {
+                $modified_input_two = str_replace(' ', '',rtrim($input, '?') );
+                if (ctype_upper($modified_input_two)) {
                     $response = 'Calm down, I know what I\'m doing!';
-                } else if (strstr($input, PHP_EOL)) {
-                    $response = 'Whatever.';
                 } else {
                     $response = 'Sure.';
                 }
-            } else {
-                $response = 'Whatever.';
-            }
         } else if ((ctype_space(substr($input, -1, 3 ))) && (strpos($input, '?'))) {
             $response = 'Sure.';
-        } else if (ctype_space(substr($input, -1, 6 ))) {
-            $response = 'Whatever.';
-        }else if(ctype_upper(rtrim(substr($input, -1, 3), '!'))) {
+        } else if(ctype_upper(rtrim(substr($input, -1, 3), '!'))) {
             $response = 'Whoa, chill out!';
         }
         return $response;
