8c246ea4d12bd9408e05555da017529b82a30cad
davidjguru <davidjguru@gmail.com>
2020-03-22 23:32:41 +0100

    (PHP) - Second Refactoring of the Bob.php class.

--- src/Bob.php
+++ src/Bob.php
@@ -10,23 +10,22 @@ class Bob {
         if (empty($input) || ctype_space($input) || ctype_cntrl($input)) {
             $response = 'Fine. Be that way!';
 
-        } else if (strpos($input, '!') && (!strpos($input, '?'))) {
-            $modified_input = str_replace(' ', '', rtrim($input, '!'));
-
-            if (ctype_upper($modified_input) || (substr_count($input, '!') > 1)  || (ctype_upper(substr($modified_input, -1, 2)))) {
+        } else if ((strpos($input, '!') && (!strpos($input, '?')))
+            && (ctype_upper(str_replace(' ', '', rtrim($input, '!')))
+            || (substr_count($input, '!') > 1)
+            || (ctype_upper(substr(str_replace(' ', '', rtrim($input, '!')), -1, 2))))
+            ||  (ctype_upper(rtrim(substr($input, -1, 3), '!')))) {
                 $response = 'Whoa, chill out!';
-            }
-        } else if (((strlen($input)) - 1) == (strpos($input, '?'))) {
-                $modified_input_two = str_replace(' ', '',rtrim($input, '?') );
-                if (ctype_upper($modified_input_two)) {
+
+        } else if ((((strlen($input)) - 1) == (strpos($input, '?')))
+            && (ctype_upper(str_replace(' ', '',rtrim($input, '?') )))) {
                     $response = 'Calm down, I know what I\'m doing!';
-                } else {
-                    $response = 'Sure.';
-                }
-        } else if ((ctype_space(substr($input, -1, 3 ))) && (strpos($input, '?'))) {
+
+        } else if (((ctype_space(substr($input, -1, 3 )))
+             && (strpos($input, '?')))
+             || ( (((strlen($input)) - 1) == (strpos($input, '?')))
+             && (!ctype_upper(str_replace(' ', '',rtrim($input, '?') ))) )) {
             $response = 'Sure.';
-        } else if(ctype_upper(rtrim(substr($input, -1, 3), '!'))) {
-            $response = 'Whoa, chill out!';
         }
         return $response;
     }
